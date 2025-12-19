e1a1f21fb3f044237ece1d75ef5c053588b10691
saymanlal <buisnesssayman@gmail.com>
2025-12-19 06:00:46 -0500

    Routing Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Features from "./pages/Features";
+import UseCases from "./pages/Use-Cases";
 import AuthPages from "./pages/AuthPages";
 import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
@@ -11,6 +13,8 @@ createRoot(document.getElementById("root")).render(
     <ThemeProvider>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/use-cases" element={<UseCases />} />
         <Route path="/auth" element={<AuthPages />} />
         {/* Add more routes here */}
         {/* <Route path="/dashboard" element={<Dashboard />} /> */}
