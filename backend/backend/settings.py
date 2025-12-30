dd7309433bcf04c8e494858b41d65ef725184311
Utkarsh Kushwaha <utkarshkushwaha246@gmail.com>
2025-12-30 16:13:51 +0530

    mera wala

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -169,14 +169,4 @@ TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 
 
 
-# =========================
-# SENDGRID
-# =========================
-SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
-SENDGRID_FROM_EMAIL = os.getenv("SENDGRID_FROM_EMAIL")
-# =========================
-# FAST2SMS
-# =========================
-FAST2SMS_API_KEY = os.getenv("FAST2SMS_API_KEY")
-OTP_PROVIDER = os.getenv("OTP_PROVIDER", "console")
 
